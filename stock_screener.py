804a78f6d37cf35ee532b2faf4cc299fc8eed161
tofipie <62646269+tofipie@users.noreply.github.com>
2025-06-05 16:48:58 +0300

    Update stock_screener.py

--- stock_screener.py
+++ stock_screener.py
@@ -94,7 +94,7 @@ results = st.write(get_ticker_news_sentiment(stock))
 # With a streamlit expander
 with st.expander("Articles"):
   for i, doc in enumerate(results):
-     st.write(i+1 : doc['text'].split('/')[-1])
+     st.write(f" Article # {i+1} : doc['text'].split('/')[-1]")
      st.write(doc['response'])
      st.write("--------------------------------")
 # Create an input box to take the user''s input question
@@ -106,7 +106,7 @@ if prompt:
 # With a streamlit expander
   with st.expander("Articles"):
     for i, doc in enumerate(results):
-      st.write(i+1 : doc['text'].split('/')[-1])
+      st.write(f" Article # {i+1} : doc['text'].split('/')[-1]")
       st.write(doc['response'])
       st.write("--------------------------------")
 
